b9e1e6123bae5f65fb6b09a8d185d1e9d32f62e8
tanvohung <tanvohung@192.168.7.104>
2019-06-29 23:33:04 +0700

    Implement AbstractPage class

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,120 @@
 package commons;
 
-public class AbstractPage {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractPage {
+	
+	/* Web Driver */
+	
+	public void openUrl(WebDriver driver, String urlValue) {
+		driver.get("urlValue");		
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();	 
+	}
+	
+	public String getCurrentPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+		
+	}
+	
+	public String getCurrentPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void refreshToPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public void acceptAlert(WebDriver driver) {
+		driver.switchTo().alert().accept();
+		
+	}
+	
+	public void cancelAlert(WebDriver driver) {
+		driver.switchTo().alert().dismiss();
+	}
+	
+	public String getTextAlert(WebDriver driver) {
+		return driver.switchTo().alert().getText();
+	}
+	
+	public void sendKeyToAlert(WebDriver driver, String value) {
+		driver.switchTo().alert().sendKeys(value);
+	}
+	
+	/* Web Element */
+	public void clickToElement(WebDriver driver, String locator) {
+		element = driver.findElement(By.xpath(locator));
+		element.click();
+	}
+	
+	public void sendKeyToElement(WebDriver driver, String locator, String value) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		element.sendKeys(value);
+	}
+	
+	public void selectItemInDropdown(WebDriver driver, String locator, String itemText) {
+		element = driver.findElement(By.xpath(locator));
+		select = new Select(element);
+		select.selectByVisibleText(itemText);
+	}
+	
+	public String getSelectedItemInDropdown(WebDriver driver, String locator) {
+		element = driver.findElement(By.xpath(locator));
+		select = new Select(element);
+		return select.getFirstSelectedOption().getText();
+	}
+	
+	public void selectItemInCustomDropdown(WebDriver driver, String parentLocator, String allItemsInDropdown, String expectedText) throws Exception {
+		  element = driver.findElement(By.xpath(parentLocator));
+		  
+		  javascriptExecutor = (JavascriptExecutor) driver;
+		  javascriptExecutor.executeScript("arguments[0].click();", element);
+		  Thread.sleep(1000);
+		  
+		  waitExplicit = new WebDriverWait(driver, 30);
+		  waitExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemsInDropdown)));
+		  
+		  elements = driver.findElements(By.xpath(allItemsInDropdown));
+		  
+		  for (WebElement childElement:elements) {
+			  if (childElement.getText().contains(expectedText)) {
+				  if(childElement.isDisplayed()) {
+					  childElement.click();
+				  }
+				  else {
+					  javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", childElement);
+					  Thread.sleep(1000);
+					  javascriptExecutor.executeScript("arguments[0].click();", childElement);
+				  }
+				  Thread.sleep(1000);
+				  break;
+			  }
+		  }
+		  
+	  }
+	
+	private WebElement element;
+	private List<WebElement> elements;
+	private Select select;
+	private JavascriptExecutor javascriptExecutor;
+	private WebDriverWait waitExplicit;
 }
