78ce466fea010cb1723beafc18a5bb24c8672a39
tanvohung <tanvohung@192.168.7.104>
2019-07-02 22:58:53 +0700

    Finish AbstractPage class

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -2,9 +2,6 @@ package commons;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -14,7 +11,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 public class AbstractPage {
 
